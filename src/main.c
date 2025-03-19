15001515e3c3b31bd148784a6c7a590ae77bf1b7
Geoffrey Jones <bashmntl@gmail.com>
2025-03-19 14:46:28 -0400

    Add basic functionality

--- src/main.c
+++ src/main.c
@@ -4,23 +4,33 @@
 
 /* Library includes. */
 #include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
 #include "timers.h"   /* Software timer related API prototypes. */
+#include"queue.h"
 
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+#define ACTIVE_HIGH true
+
 #define DEMAND_1_GPIO  2
-#define DEMAND_2_GPIO  3
+#define DEMAND_2_GPIO  ( DEMAND_2_GPIO + 1 )
+
+#define PUMP_1_GPIO 18
+#define PUMP_2_GPIO ( PUMP_1_GPIO +1 )
+
+#define BOILER_GPIO 16
 
 #define LED_TIMER_PERIOD  pdMS_TO_TICKS( 500UL ) 
 #define STATUS_LED  ( PICO_DEFAULT_LED_PIN )
 
-#define DEBOUNCE_TIMER_PERIOD pdMS_TO_TICKS( 50UL)
+#define DEBOUNCE_TIMER_PERIOD pdMS_TO_TICKS( 1000UL)
 
 typedef struct {
-  uint gpio_pin;
-  uint pin_state;
+  uint input_gpio;
+  uint output_gpio;
+  uint desired_state;
 } Demand_State_t;
 
 static char event_str[128];
@@ -32,25 +42,46 @@ static void debounceCallback(TimerHandle_t timer);
 static void gpioIRQCallback(uint gpio, uint32_t events);
 static void gpioEventString(char *buf, uint32_t events);
 
+static void demandQueueRXTask(void *params);
+static void startupTask(void *params);
+static TaskHandle_t startup_task;
+
 #define NUM_DEMAND_INPUTS 2
 static TimerHandle_t debounce_timer[NUM_DEMAND_INPUTS];
 static Demand_State_t demand_state[NUM_DEMAND_INPUTS];
+// static TimerHandle_t shutoff_timer[NUM_DEMAND_INPUTS];
+
+static QueueHandle_t demand_queue = NULL;
+#define DEMAND_QUEUE_RX_TASK_PRIORITY	( tskIDLE_PRIORITY + 2 )
+#define STARTUP_TASK_PRIORITY ( tskIDLE_PRIORITY + 1 )
 
 int main(){
   setupHardware();
-  printf(" Starting pump_controller.\n");
+  printf("initializing pump_controller.\n");
   
   // TODO: move this
   for(uint i = 0; i < NUM_DEMAND_INPUTS; i++){
     debounce_timer[i]= NULL;
-    demand_state[i].gpio_pin = DEMAND_1_GPIO + i;
-    demand_state[i].pin_state = gpio_get(DEMAND_1_GPIO + i);
+    demand_state[i].input_gpio = DEMAND_1_GPIO + i;
+    demand_state[i].desired_state = gpio_get(DEMAND_1_GPIO + i);
+    demand_state[i].output_gpio = PUMP_1_GPIO + i;
+  }
+
+  demand_queue =
+      xQueueCreate(NUM_DEMAND_INPUTS,
+                   sizeof(Demand_State_t)); // TODO: how big should our queue be
+  if (demand_queue != NULL) {
+    xTaskCreate(demandQueueRXTask, "DemandRx", configMINIMAL_STACK_SIZE, NULL,
+                DEMAND_QUEUE_RX_TASK_PRIORITY, NULL);
+
+    xTaskCreate(startupTask, "Startup", configMINIMAL_STACK_SIZE, NULL,
+                STARTUP_TASK_PRIORITY, &startup_task);
   }
 
   TimerHandle_t LEDTimer = NULL;
   createTimer(&LEDTimer);
   xTimerStart(LEDTimer, 0);
-// create tasks for each input
+
   vTaskStartScheduler();
   for( ;; ){
       
@@ -65,9 +96,20 @@ static void setupHardware( void )
     gpio_set_dir(PICO_DEFAULT_LED_PIN, 1);
     gpio_put(PICO_DEFAULT_LED_PIN, !PICO_DEFAULT_LED_PIN_INVERTED);
 
-    gpio_init(DEMAND_1_GPIO);
-    gpio_set_dir(DEMAND_1_GPIO, false);
-    gpio_set_irq_enabled_with_callback(DEMAND_1_GPIO, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &gpioIRQCallback);
+    for (uint i = 0; i < NUM_DEMAND_INPUTS; i++) {
+      gpio_init(DEMAND_1_GPIO + i);
+      gpio_init(PUMP_1_GPIO + i);
+      gpio_set_dir(DEMAND_1_GPIO + i, false);
+      gpio_set_dir(PUMP_1_GPIO + i, true);
+      gpio_set_irq_enabled_with_callback(
+          DEMAND_1_GPIO + i, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true,
+          &gpioIRQCallback);
+      gpio_put(PUMP_1_GPIO + i, 0);
+    }
+
+    gpio_put(BOILER_GPIO, (false == ACTIVE_HIGH));
+    gpio_init(BOILER_GPIO);
+    gpio_set_dir(BOILER_GPIO, true);
 }
 
 static void createTimer(TimerHandle_t *timer)
@@ -97,14 +139,14 @@ static void timerCallback( TimerHandle_t timer )
 }
 
 static void gpioIRQCallback(uint gpio, uint32_t events){
-  // TODO: debounce interrupts
-  // only put something in the queue if the state has changed and remains stable for some amount of time
   gpio_acknowledge_irq(gpio, events);
   gpioEventString(event_str, events);
   uint gpio_state = gpio_get(gpio);
   printf("GPIO %d %s %d\n", gpio, event_str, gpio_state);
 
   uint index = gpio - DEMAND_1_GPIO;
+  
+  // TODO: we could make this a function which we pass in a timer,period and callback
   TimerHandle_t timer = debounce_timer[index];
   if( timer != NULL){
     printf("stopping timer %d \n", index);
@@ -122,15 +164,48 @@ static void debounceCallback(TimerHandle_t timer){
   (void)timer;
   uint index = (uint) pvTimerGetTimerID(timer);
   Demand_State_t state = demand_state[index];
-  uint gpio_state = gpio_get(state.gpio_pin);
+  uint gpio_state = gpio_get(state.input_gpio);
   printf("debounce callback %d %d\n", index, gpio_state);
 
-  if( gpio_state != state.pin_state){
-    printf("do something for demand %d %d\n",index, gpio_state);
-    demand_state[index].pin_state = gpio_state;
+  if( gpio_state != state.desired_state){
+    printf("queuing demand %d %d\n",index, gpio_state);
+    demand_state[index].desired_state = gpio_state;
+    xQueueSendToBackFromISR(demand_queue, &demand_state[index], 0);
   }
 }
-  
+ 
+static void demandQueueRXTask(void *params){
+  Demand_State_t state;
+  (void)params;
+
+  for(;;){
+    xQueueReceive(demand_queue, &state, portMAX_DELAY);
+    printf("set pump %d state %d, \n",state.output_gpio, state.desired_state);
+    gpio_put( state.output_gpio,( state.desired_state == ACTIVE_HIGH));
+
+    // if the desired state is going to off, set a timer to actually turn the pump off
+    // if any of the desired states is on, turn the boiler on
+    uint desired_boiler_state = 0;
+    for(uint i = 0; i < NUM_DEMAND_INPUTS; i++){
+      if(demand_state[i].desired_state == 1){
+        desired_boiler_state = 1;
+        break;
+      }
+    }
+    gpio_put(BOILER_GPIO,(desired_boiler_state == ACTIVE_HIGH));
+  }
+}
+
+static void startupTask(void *params){
+  (void)params;
+  printf("startup task \n");
+  for(uint i =0 ; i < NUM_DEMAND_INPUTS ; i++){
+    xQueueSendToBack(demand_queue,
+      &demand_state[i],
+      0);
+  }
+  while(1);
+}
 
 static const char *gpio_irq_str[] = {
   "LEVEL_LOW",  // 0x1
