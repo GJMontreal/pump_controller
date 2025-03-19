a29ad46a7fb9c0abfed8ebadcbbac19721245c27
Geoffrey Jones <bashmntl@gmail.com>
2025-03-19 15:58:36 -0400

    Turn the pumps off after a delay. The boiler shuts down immmediately

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 /* Library includes. */
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
+#include "projdefs.h"
 #include "timers.h"   /* Software timer related API prototypes. */
 #include"queue.h"
 
@@ -12,7 +13,8 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-#define ACTIVE_HIGH true
+#define OUTPUT_ACTIVE_HIGH true
+#define INPUT_ACTIVE_HIGH true
 
 #define DEMAND_1_GPIO  2
 #define DEMAND_2_GPIO  ( DEMAND_2_GPIO + 1 )
@@ -26,19 +28,26 @@
 #define STATUS_LED  ( PICO_DEFAULT_LED_PIN )
 
 #define DEBOUNCE_TIMER_PERIOD pdMS_TO_TICKS( 1000UL)
-
+#define PURGE_TIME pdMS_TO_TICKS( 5000UL )
 typedef struct {
   uint input_gpio;
   uint output_gpio;
   uint desired_state;
-} Demand_State_t;
+  TimerHandle_t shutoff_timer;
+} Demand_t;
 
 static char event_str[128];
 
 static void setupHardware(void);
 static void createTimer(TimerHandle_t* timer);
+
+static void setupShutdownTimers();
+
+// FreeRTOS callbacks
 static void timerCallback( TimerHandle_t timer );
 static void debounceCallback(TimerHandle_t timer);
+static void shutdownCallback(TimerHandle_t timer);
+
 static void gpioIRQCallback(uint gpio, uint32_t events);
 static void gpioEventString(char *buf, uint32_t events);
 
@@ -48,8 +57,7 @@ static TaskHandle_t startup_task;
 
 #define NUM_DEMAND_INPUTS 2
 static TimerHandle_t debounce_timer[NUM_DEMAND_INPUTS];
-static Demand_State_t demand_state[NUM_DEMAND_INPUTS];
-// static TimerHandle_t shutoff_timer[NUM_DEMAND_INPUTS];
+static Demand_t demand[NUM_DEMAND_INPUTS];
 
 static QueueHandle_t demand_queue = NULL;
 #define DEMAND_QUEUE_RX_TASK_PRIORITY	( tskIDLE_PRIORITY + 2 )
@@ -62,16 +70,18 @@ int main(){
   // TODO: move this
   for(uint i = 0; i < NUM_DEMAND_INPUTS; i++){
     debounce_timer[i]= NULL;
-    demand_state[i].input_gpio = DEMAND_1_GPIO + i;
-    demand_state[i].desired_state = gpio_get(DEMAND_1_GPIO + i);
-    demand_state[i].output_gpio = PUMP_1_GPIO + i;
+    demand[i].input_gpio = DEMAND_1_GPIO + i;
+    demand[i].desired_state = gpio_get(DEMAND_1_GPIO + i);
+    demand[i].output_gpio = PUMP_1_GPIO + i;
   }
+
+  setupShutdownTimers();
 
   demand_queue =
       xQueueCreate(NUM_DEMAND_INPUTS,
-                   sizeof(Demand_State_t)); // TODO: how big should our queue be
+                   sizeof(Demand_t));
   if (demand_queue != NULL) {
-    xTaskCreate(demandQueueRXTask, "DemandRx", configMINIMAL_STACK_SIZE, NULL,
+    xTaskCreate(demandQueueRXTask, "DemandRx", configMINIMAL_STACK_SIZE, &demand,
                 DEMAND_QUEUE_RX_TASK_PRIORITY, NULL);
 
     xTaskCreate(startupTask, "Startup", configMINIMAL_STACK_SIZE, NULL,
@@ -107,7 +117,7 @@ static void setupHardware( void )
       gpio_put(PUMP_1_GPIO + i, 0);
     }
 
-    gpio_put(BOILER_GPIO, (false == ACTIVE_HIGH));
+    gpio_put(BOILER_GPIO, (false == OUTPUT_ACTIVE_HIGH));
     gpio_init(BOILER_GPIO);
     gpio_set_dir(BOILER_GPIO, true);
 }
@@ -130,6 +140,14 @@ to anything. */
 the LED off. */
 timerCallback
 );
+}
+
+static void setupShutdownTimers(){
+  char timer_name[12];
+  for(uint i=0; i < NUM_DEMAND_INPUTS; i++){
+    sprintf(timer_name, "timer %d", i);
+    demand[i].shutoff_timer = xTimerCreate(timer_name, PURGE_TIME, pdFALSE, (void*)&demand[i], &shutdownCallback);
+  }
 }
 
 static void timerCallback( TimerHandle_t timer )
@@ -161,38 +179,50 @@ static void gpioIRQCallback(uint gpio, uint32_t events){
 }
 
 static void debounceCallback(TimerHandle_t timer){
-  (void)timer;
   uint index = (uint) pvTimerGetTimerID(timer);
-  Demand_State_t state = demand_state[index];
+  Demand_t state = demand[index];
+  debounce_timer[index] = NULL;
   uint gpio_state = gpio_get(state.input_gpio);
   printf("debounce callback %d %d\n", index, gpio_state);
 
   if( gpio_state != state.desired_state){
     printf("queuing demand %d %d\n",index, gpio_state);
-    demand_state[index].desired_state = gpio_state;
-    xQueueSendToBackFromISR(demand_queue, &demand_state[index], 0);
+    demand[index].desired_state = gpio_state;
+    xQueueSendToBackFromISR(demand_queue, &demand[index], 0);
   }
 }
- 
+
+// TODO: how could you pass in the array of demands to the callback
+static void shutdownCallback(TimerHandle_t timer){
+  Demand_t* demand = (Demand_t*) pvTimerGetTimerID(timer);
+  gpio_put(demand->output_gpio, (demand->desired_state == OUTPUT_ACTIVE_HIGH)); 
+}
+
+//how do you pass in parameters
 static void demandQueueRXTask(void *params){
-  Demand_State_t state;
+  Demand_t pump_demand;
   (void)params;
+  Demand_t *demands = (Demand_t*)params;
 
   for(;;){
-    xQueueReceive(demand_queue, &state, portMAX_DELAY);
-    printf("set pump %d state %d, \n",state.output_gpio, state.desired_state);
-    gpio_put( state.output_gpio,( state.desired_state == ACTIVE_HIGH));
-
-    // if the desired state is going to off, set a timer to actually turn the pump off
-    // if any of the desired states is on, turn the boiler on
+    xQueueReceive(demand_queue, &pump_demand, portMAX_DELAY);
+  
+    printf("set pump %d state %d, \n",pump_demand.output_gpio, pump_demand.desired_state);
+    if(pump_demand.desired_state == 0 ){
+      xTimerStart(pump_demand.shutoff_timer, 0);    
+    }else{
+      xTimerStop(pump_demand.shutoff_timer, 0);
+      gpio_put( pump_demand.output_gpio,( pump_demand.desired_state == OUTPUT_ACTIVE_HIGH));
+    }
+  
     uint desired_boiler_state = 0;
     for(uint i = 0; i < NUM_DEMAND_INPUTS; i++){
-      if(demand_state[i].desired_state == 1){
+      if((demands+i)->desired_state == 1){
         desired_boiler_state = 1;
         break;
       }
     }
-    gpio_put(BOILER_GPIO,(desired_boiler_state == ACTIVE_HIGH));
+    gpio_put(BOILER_GPIO,(desired_boiler_state == OUTPUT_ACTIVE_HIGH));
   }
 }
 
@@ -201,7 +231,7 @@ static void startupTask(void *params){
   printf("startup task \n");
   for(uint i =0 ; i < NUM_DEMAND_INPUTS ; i++){
     xQueueSendToBack(demand_queue,
-      &demand_state[i],
+      &demand[i],
       0);
   }
   while(1);
