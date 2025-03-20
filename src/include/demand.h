11262f1fd0eed44d4ec204a6728465619e95a6e3
Geoffrey Jones <bashmntl@gmail.com>
2025-03-20 11:16:04 -0400

    formatting

--- src/include/demand.h
+++ src/include/demand.h
@@ -4,14 +4,14 @@
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 
+#include "pico/stdlib.h"
 #include "queue.h"
 #include "timers.h"
-#include "pico/stdlib.h"
 
-#define PURGE_TIME pdMS_TO_TICKS( 5000UL )
+#define PURGE_TIME pdMS_TO_TICKS(5000UL)
 #define NUM_DEMAND_INPUTS 2
 
-#define DEMAND_QUEUE_RX_TASK_PRIORITY	( tskIDLE_PRIORITY + 2 )
+#define DEMAND_QUEUE_RX_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
 
 typedef struct {
   uint input_gpio;
@@ -29,8 +29,7 @@ typedef struct {
 extern QueueHandle_t demand_queue;
 extern Demand_t demand[];
 
-
-void initDemands(Demand_t* demand);
+void initDemands(Demand_t *demand);
 void demandQueueRXTask(void *params);
 void shutdownCallback(TimerHandle_t timer);
 
