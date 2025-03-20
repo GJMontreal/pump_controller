5c917e08d0e3f243a7af74c6e48006d30030a520
Geoffrey Jones <bashmntl@gmail.com>
2025-03-20 11:14:45 -0400

    formatting

--- src/include/gpio.h
+++ src/include/gpio.h
@@ -3,16 +3,16 @@
 
 #include "pico/stdlib.h"
 
-#define DEBOUNCE_TIMER_PERIOD pdMS_TO_TICKS( 1000UL)
+#define DEBOUNCE_TIMER_PERIOD pdMS_TO_TICKS(1000UL)
 
 #define OUTPUT_ACTIVE_HIGH true
 #define INPUT_ACTIVE_HIGH true
 
-#define DEMAND_1_GPIO  2
-#define DEMAND_2_GPIO  ( DEMAND_2_GPIO + 1 )
+#define DEMAND_1_GPIO 2
+#define DEMAND_2_GPIO (DEMAND_2_GPIO + 1)
 
 #define PUMP_1_GPIO 18
-#define PUMP_2_GPIO ( PUMP_1_GPIO +1 )
+#define PUMP_2_GPIO (PUMP_1_GPIO + 1)
 
 #define BOILER_GPIO 16
 
